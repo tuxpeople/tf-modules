ef6aa3382074e1a210690954bad4e9815f964b75
Thomas Deutsch <thomas@tuxpeople.org>
2022-08-26 11:26:22 +0200

    adding prepare script

---
+++ prepare.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ROOT_DIR=$(git rev-parse --show-toplevel)
+
+for platform in $(find "${ROOT_DIR}"  -type d -mindepth 1 -maxdepth 1 -not -path '*/\.git/*'); do
+    for module in $(find "${platform}"  -type d -mindepth 1 -maxdepth 1 -not -path '*/\.git/*'); do
+        cd "${module}"
+        echo "Working on ${module}"
+        echo "Formating Terraform"
+        terraform fmt -recursive
+        echo "Generating docs"
+        terraform-docs markdown table --output-file README.md --output-mode inject ./
+    done
+done
