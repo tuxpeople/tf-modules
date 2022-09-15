2d759b394de80009a4c6ed891abee6840ebd8816
Thomas Deutsch <thomas@tuxpeople.org>
2022-09-15 21:47:00 +0200

    module for vsphere

---
+++ vsphere/vsphere_vm/main.tf
@@ -0,0 +1,82 @@
+locals {
+  user_data  = var.user_data != "" ? var.user_data : "${path.module}/files/cloud-init.tftpl"
+  clean_name = substr(lower(replace(var.hostname, "/[[:^alnum:]]/", "")), 0, 60)
+}
+
+data "vsphere_datacenter" "main" {
+  count = var.instances_count
+  name = var.datacenter
+}
+
+data "vsphere_compute_cluster" "main" {
+  count = var.instances_count
+  name          = var.cluster
+  datacenter_id = data.vsphere_datacenter.main[count.index].id
+}
+
+data "vsphere_datastore" "main" {
+  count = var.instances_count
+  name          = var.datastore
+  datacenter_id = data.vsphere_datacenter.main[count.index].id
+}
+
+data "vsphere_network" "main" {
+  count = var.instances_count
+  name          = var.network
+  datacenter_id = data.vsphere_datacenter.main[count.index].id
+}
+
+data "vsphere_virtual_machine" "template" {
+  count = var.instances_count
+  name          = var.template
+  datacenter_id = data.vsphere_datacenter.main[count.index].id
+}
+
+resource "vsphere_virtual_machine" "main" {
+  count = var.instances_count
+
+  resource_pool_id = data.vsphere_compute_cluster.main[count.index].resource_pool_id
+  datastore_id     = data.vsphere_datastore.main[count.index].id
+  folder           = var.folder
+
+  name     = (var.instances_count == "1" ? "${local.clean_name}" : "${local.clean_name}${(count.index + 1)}")
+  num_cpus = var.vCPU
+  memory   = var.vMEM
+  guest_id = data.vsphere_virtual_machine.template[count.index].guest_id
+  wait_for_guest_net_timeout = 0
+
+  cdrom {
+    client_device = true
+  }
+
+  network_interface {
+    network_id = data.vsphere_network.main[count.index].id
+  }
+
+  disk {
+    label            = "disk0"
+    size             = var.disksize
+    eagerly_scrub    = var.eagerly_scrub
+    thin_provisioned = var.thin_provisioned
+  }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template[count.index].id
+  }
+
+  vapp {
+    properties = {
+      user-data = base64encode(templatefile(local.user_data, ({
+        pubkey      = file(pathexpand(var.ssh_public_keyfile))
+        hostname    = (var.instances_count == "1" ? "${local.clean_name}" : "${local.clean_name}${(count.index + 1)}")
+        instance_id = (var.instances_count == "1" ? "${local.clean_name}" : "${local.clean_name}${(count.index + 1)}")
+      })))
+      hostname    = "${local.clean_name}"
+      instance-id = "${local.clean_name}"
+    }
+  }
+  
+  provisioner "local-exec" {
+    command = "fix-ssh-key ${local.clean_name}"
+  }
+}
---
+++ vsphere/vsphere_vm/variables.tf
@@ -0,0 +1,72 @@
+variable "instances_count" {
+  description = "How many of those VM instances? (Default: 1)"
+  type = number
+  default     = "1"
+}
+variable "vCPU" {
+  description = "How many vCPU"
+  type = number
+}
+variable "vMEM" {
+  description = "How many vMEM"
+  type = number
+}
+variable "disksize" {
+  description = "Disksize in GB"
+  type = number
+}
+variable "thin_provisioned" {
+  description = "How many vCPU"
+  type = string
+}
+variable "eagerly_scrub" {
+  description = "Eagerly scrub disk (Defaults to: true)"
+  type = bool
+  default = true
+}
+variable "folder" {
+  description = "Folder where the VMs should be placed (must exist)"
+  type = string
+}
+variable "datastore" {
+  description = "Datastore where the VMs should be placed (must exist)"
+  type = string
+}
+variable "network" {
+  description = "Network which the VM should be connected to (must exist)"
+  type = string
+}
+variable "template" {
+  description = "Which template to clone. (Defaults to: focal-server-cloudimg-amd64)"
+  type = string
+    default         = "focal-server-cloudimg-amd64"
+}
+variable "cluster" {
+  description = "Cluster where the VMs should be placed (Defaults to: Homelab)"
+  type = string
+  default = "Homelab"
+}
+variable "datacenter" {
+  description = "Datacenter where the VMs should be placed (Defaults to: SKY)"
+  type = string
+  default "SKY"
+}
+variable "hostname" {
+  description = "Hostname of the VM, will be appended with a number depending on how many VMs beeing created."
+  type = string
+}
+variable "user_data" {
+  description = "Supply a special cloud-init file. If empty, the module's default will be used."
+  type = string
+  default = ""
+}
+variable "guest_id" {
+  description = "guest_id of the VM (Defaults to: ubuntu64Guest)"
+  type = string
+  default = "ubuntu64Guest"
+}
+variable "ssh_public_keyfile" {
+  description = "SSH public keyfile for the VMs (Default: ~/.ssh/id_rsa.pub)"
+  type        = string
+  default     = "~/.ssh/id_rsa.pub"
+}
