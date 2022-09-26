0427822f98ac72333f9f50e9665dab8e58d8eaa5
Thomas Deutsch <thomas@tuxpeople.org>
2022-09-26 14:53:20 +0200

    fix

--- vsphere/vsphere_vm/main.tf
+++ vsphere/vsphere_vm/main.tf
@@ -95,6 +95,6 @@ resource "vsphere_virtual_machine" "main" {
   }
 
   provisioner "local-exec" {
-    command = "while ! nc -z ${(var.instances_count == "1" ? "${var.hostname}" : "${format("${var.hostname}%02s", (count.index + 1))}")} 22; do sleep 10; done; fix-ssh-key ${(var.instances_count == "1" ? "${var.hostname}" : "${format("${var.hostname}%02s", (count.index + 1))}")}; ssh ansible@${(var.instances_count == "1" ? "${var.hostname}" : "${format("${var.hostname}%02s", (count.index + 1))}")} 'cloud-init status --wait > /dev/null'"
+    command = "while ! nc -z ${(var.instances_count == "1" ? "${var.hostname}" : "${format("${var.hostname}%02s", (count.index + 1))}")} 22; do sleep 10; done; fix-ssh-key ${(var.instances_count == "1" ? "${var.hostname}" : "${format("${var.hostname}%02s", (count.index + 1))}")}; ssh ansible@${(var.instances_count == "1" ? "${var.hostname}" : "${format("${var.hostname}%02s", (count.index + 1))}")} 'cloud-init status --wait > /dev/null'; sleep 20"
   }
 }
