a1f63ec5d949811f0a81c30116088b28f4252e28
Thomas Deutsch <thomas@tuxpeople.org>
2022-09-30 14:27:42 +0200

    adding unifi/dns

---
+++ unifi/dns/main.tf
@@ -0,0 +1,49 @@
+locals {
+  dns_config = templatefile(
+    "${path.module}/files/dns.tftpl",
+    {
+      serverrecord = var.serverrecord
+      addressrecord = var.addressrecord
+      hostrecord = var.hostrecord
+      cnamerecord = var.cnamerecord
+    }
+  )
+  dns_file = "/mnt/data/dns.d/${random_pet.pet.id}.dns"
+}
+
+resource "random_pet" "pet" {
+  length = 3
+}
+
+resource "null_resource" "copy-dns" {
+  triggers = {
+    foobar = local.dns_config
+  }
+
+  provisioner "file" {
+    content     = local.dns_config
+    destination = "${local.dns_file}"
+
+    connection {
+      type        = "ssh"
+      user        = "${var.sshuser}"
+      private_key = file(pathexpand("${var.sshkey}"))
+      host        = "${var.dnsserver}"
+    }
+  }
+
+  provisioner "local-exec" {
+    command = "ssh ${var.sshuser}@${var.dnsserver} 'rm -f ${local.dns_file} && /mnt/data/on_boot.d/11-create-local-dns-conf.sh'"
+    when    = destroy
+  }
+}
+
+resource "null_resource" "refresh-dns" {
+  depends_on = [
+    null_resource.rancher-test-dns
+  ]
+
+  provisioner "local-exec" {
+    command = "ssh ${var.sshuser}@${var.dnsserver} /mnt/data/on_boot.d/11-create-local-dns-conf.sh"
+  }
+}
---
+++ unifi/dns/variables.tf
@@ -0,0 +1,41 @@
+variable "serverrecord" {
+  description = "Map of server records (Defaults to {}) Eg: {name = \"Mabel\", age = 52}"
+  type        = map
+  default = "{}"
+}
+
+variable "addressrecord" {
+  description = "Map of address records (Defaults to {}) Eg: {name = \"Mabel\", age = 52}"
+  type        = map
+  default = "{}"
+}
+
+variable "hostrecord" {
+  description = "Map of host records (Defaults to {}) Eg: {name = \"Mabel\", age = 52}"
+  type        = map
+  default = "{}"
+}
+
+variable "cnamerecord" {
+  description = "Map of cname records (Defaults to {}) Eg: {name = \"Mabel\", age = 52}"
+  type        = map
+  default = "{}"
+}
+
+variable "sshkey" {
+  description = "Path to ssh-key (Defaults to ~/.ssh/id_rsa)"
+  type        = string
+  default = "~/.ssh/id_rsa"
+}
+
+variable "sshuser" {
+  description = "SSH Username (Defaults to root)"
+  type        = string
+  default = "root"
+}
+
+variable "dnsserver" {
+  description = "Host of DNS server (Defaults to 10.20.30.1)"
+  type        = string
+  default = "10.20.30.1"
+}
