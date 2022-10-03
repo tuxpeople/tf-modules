d122c1d9ff114e08ef8907267ced251598268f6c
Thomas Deutsch <thomas@tuxpeople.org>
2022-10-03 10:13:51 +0200

    fix

--- vsphere/vsphere_vm/main.tf
+++ vsphere/vsphere_vm/main.tf
@@ -104,5 +104,6 @@ resource "vsphere_virtual_machine" "main" {
 resource "null_resource" "cleanup_ssh_keys" {
   count = var.instances_count
   provisioner "local-exec" {
-    command = "ssh-keygen -R ${(var.instances_count == "1" ? "${var.hostname}" : "${format("${var.hostname}%02s", (count.index + 1))}; ssh-keygen -R ${(var.instances_count == "1" ? "${var.hostname}" : "${format("${var.hostname}%02s", (count.index + 1))}.${var.domain})}; ssh-keygen -R ${vsphere_virtual_machine.main[count.index].default_ip_address}"  }
+    command = "ssh-keygen -R ${(var.instances_count == "1" ? "${var.hostname}" : "${format("${var.hostname}%02s", (count.index + 1))}")}; ssh-keygen -R ${(var.instances_count == "1" ? "${var.hostname}" : "${format("${var.hostname}%02s", (count.index + 1))}")}.${var.domain})}; ssh-keygen -R ${vsphere_virtual_machine.main[count.index].default_ip_address}"
 }
+  }
