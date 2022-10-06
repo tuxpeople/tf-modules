5ed98fc3809f3ca4daeca95908032b58cc842f75
Thomas Deutsch <thomas@tuxpeople.org>
2022-10-06 16:33:57 +0200

    add cluster_token output

--- rancher/rke2/outputs.tf
+++ rancher/rke2/outputs.tf
@@ -6,3 +6,7 @@ output "RKE2 Releases" {
   description = "Map of latest RKE2 releases per channel."
   value       = local.rke2_list
 }
+output "cluster_token" {
+  description = "Map of latest RKE2 releases per channel."
+  value       = random_password.cluster-token.result
+}
