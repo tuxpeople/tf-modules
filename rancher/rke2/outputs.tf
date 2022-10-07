8bd514d6a92daa7f4347b1a5c349f6ca04b4f3d0
Thomas Deutsch <thomas@tuxpeople.org>
2022-10-07 14:36:02 +0200

    add kubeconfig_path output

--- rancher/rke2/outputs.tf
+++ rancher/rke2/outputs.tf
@@ -2,3 +2,7 @@ output "cluster_token" {
   description = "Map of latest RKE2 releases per channel."
   value       = random_password.cluster-token.result
 }
+output "kubeconfig_path" {
+  description = "Path of the kubeconfig"
+  value       = var.kubeconfig_path
+}
