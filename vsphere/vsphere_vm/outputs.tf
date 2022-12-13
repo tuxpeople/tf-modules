65a7224c6960371b705555d245b7a4180cccc6f2
Thomas Deutsch <thomas@tuxpeople.org>
2022-12-13 10:27:47 +0100

    maybe this fixes it

--- vsphere/vsphere_vm/outputs.tf
+++ vsphere/vsphere_vm/outputs.tf
@@ -1,24 +1,24 @@
 output "VM" {
   description = "VM Names"
-  value       = vsphere_virtual_machine.*.*.name
+  value       = try(vsphere_virtual_machine.local.*.name, vsphere_virtual_machine.ovf.*.name)
 }
 
 output "ip" {
   description = "default ip address of the deployed VM"
-  value       = vsphere_virtual_machine.*.*.default_ip_address
+  value       = try(vsphere_virtual_machine.local.*.default_ip_address, vsphere_virtual_machine.ovf.*.default_ip_address)
 }
 
 output "guest-ip" {
   description = "all the registered ip address of the VM"
-  value       = vsphere_virtual_machine.*.*.guest_ip_addresses
+  value       = try(vsphere_virtual_machine.local.*.guest_ip_addresses, vsphere_virtual_machine.ovf.*.guest_ip_addresses)
 }
 
 output "uuid" {
   description = "UUID of the VM in vSphere"
-  value       = vsphere_virtual_machine.*.*.uuid
+  value       = try(vsphere_virtual_machine.local.*.uuid, vsphere_virtual_machine.ovf.*.uuid)
 }
 
 output "disk" {
   description = "Disks of the deployed VM"
-  value       = vsphere_virtual_machine.*.*.disk
+  value       = try(vsphere_virtual_machine.local.*.disk, vsphere_virtual_machine.ovf.*.disk)
 }
